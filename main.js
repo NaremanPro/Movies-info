7675614cdcff76c2626d5c4094b9c854e485a67d
Sahar <saharsaker6gmail.com>
2019-04-03 18:36:48 +0300

    changing in js file

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// We create a var to hold the constant url .
+// // We create a var to hold the constant url .
 var imgUrl = "https://image.tmdb.org/t/p/w185_and_h278_bestv2/";
 
 // Create a function that take the value of the input search
@@ -11,68 +11,52 @@ function getMovieData (event) {
     .then(function(response) {
       return response.json();
     })
-
     .then(function(data) {
-      var result1 = data.results[0];
-
-      document.getElementById("title1").textContent = result1.title;
-      document.getElementById("image1").src = imgUrl + result1.poster_path;
-      document.getElementById("date1").textContent = result1.release_date;
-      document.getElementById("summary1").textContent = result1.overview;
-      document.getElementById("average1").textContent = result1.vote_average;
-
-      var result2 = data.results[1] ;
-
-      document.getElementById("title2").textContent = result2.title;
-      document.getElementById("image2").src = imgUrl + result2.poster_path;
-      document.getElementById("date2").textContent = result2.release_date;
-      document.getElementById("summary2").textContent = result2.overview;
-      document.getElementById("average2").textContent = result2.vote_average;
-
-      var result3 = data.results[2] ;
-
-      document.getElementById("title3").textContent = result3.title;
-      document.getElementById("image3").src = imgUrl + result3.poster_path;
-      document.getElementById("date3").textContent = result3.release_date;
-      document.getElementById("summary3").textContent = result3.overview;
-      document.getElementById("average3").textContent = result3.vote_average;
-
-})
-    //console.log(data);
-
+       var informationArr = [data.results[0], data.results[1], data.results[2]];
+       informationArr.forEach(function(el, indx){
+         document.getElementById("title" + (indx + 1)).textContent = el.title;
+         document.getElementById("date" + (indx + 1)).textContent = el.release_date;
+         document.getElementById("summary" + (indx + 1)).textContent = el.overview;
+         document.getElementById("average" + (indx + 1)).textContent = el.vote_average;
+       })
+       .then(function(data) {
+      informationArr.forEach(function(el, indx){
+        document.getElementById("image" + (indx + 1)).src = imgUrl + el.poster_path;
+      })
+    })
+  })
   .catch(function(error) {
     console.log(error);
   })
 }
-// Adding a callback to have a click
-document.getElementById("buttonSearch").addEventListener("click", getMovieData);
 
 // ************************************************************************
 
 // this is the second api
 function movieInfo(event) {
-  // event.preventDefault();
+  event.preventDefault();
   var searchValue = document.getElementById("search").value;
-  console.log(searchValue);
-  var arrId = ["Title", "Website", "Runtime", "Genre", "Director", "Writer", "Actors", "Language", "Country", "Awards", "Type", "BoxOffice", "Production"];
+  console.log("search = ", searchValue);
+  var arrId = ["Title", "Runtime", "Genre", "Director", "Writer", "Actors", "Language", "Country", "Awards", "Type", "BoxOffice", "Production"];
   console.log(arrId);
   fetch("http://www.omdbapi.com/?t=" + searchValue +"&apikey=372f4e36")
     .then(function(response) {
       return response.json();
     })
     .then(function(data) {
-      // var val = Object.values();
-      // console.log(val);
-      // var keys = data.Object.keys();
-      // console.log(keys);
-      // var index = 0;
+      console.log("movieInfo: ", data)
       arrId.forEach(function(el) {
-      // index = keys.ingexOf(el);
-      document.getElementById(el).textContent = data.el;
+
+      console.log(data[el]);
+      document.getElementById(el).textContent = data[el];
+
      })
+
     // .catch(function(error) {
     //   console.log(error);
     //  })
  })
 }
+
 document.getElementById("buttonInfo").addEventListener("click", movieInfo);
+document.getElementById("buttonSearch").addEventListener("click", getMovieData);
